424462cd5186583bca385ea9b3dc7e1f70ee6dbf
Ravenovus <voronov213@gmail.com>
2026-06-17 11:51:50 +0300

    initial classes definition

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { compareAsc, format } from "date-fns";
+
+//ACTUAL TODO : 
+//start from drawing out the app
+//shape it out in html and css
+//create classes and arrays for projects and tasks
+//check how storage works and how to check for existence
---
+++ src/task.js
@@ -0,0 +1,27 @@
+export class Task{
+    _name;
+    _priority;
+    _dueDate;
+    _status;
+    _taskId;
+
+    constructor(name,priority,date){
+        this._name = name;
+        this._priority = priority; //probably needs a function of its own to convert into numeric, OR just keep as str, that works too
+        this._dueDate = date;
+        this._status = 0;
+        this._taskId = crypto.randomUUID();
+    }
+
+    get name(){return this._name;}
+    get priority(){return this._priority;}
+    get dueDate(){return this._dueDate;}
+    get status(){return this._status;}
+    get taskId(){return this._taskId;}
+
+    set priority(value){this._priority = value;}
+    set dueDate(value){this._dueDate = value;}
+    set status(value){this._status = value;}
+
+
+}
